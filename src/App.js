c79253a3d85c7569875e32b134b8061446deb8af
unknown <ixentrik@gmail.com>
2019-12-17 14:51:34 -0800

    fixesCurrencyUndefined

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
 import slugify from "slugify";
-
+import FeatureItem from "./FeatureItem/FeatureItem";
 import "./App.css";
 
 // This object will allow us to
@@ -50,17 +50,7 @@ class App extends Component {
         const itemHash = slugify(JSON.stringify(item));
         return (
           <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
+            <FeatureItem />
           </div>
         );
       });
--- src/FeatureItem/FeatureItem.js
+++ src/FeatureItem/FeatureItem.js
@@ -1,6 +1,12 @@
 import React from "react";
+import slugify from "slugify";
 
 function FeatureItem(props) {
+  const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+  const itemHash = slugify(JSON.stringify(item));
   return (
     <div>
       <input
@@ -9,7 +15,7 @@ function FeatureItem(props) {
         className="feature__option"
         name={slugify(props.feature)}
         checked={props.checked}
-        onChange={e => props.updateFeature(event, props.feature, props.item)}
+        onChange={e => props.updateFeature(e, props.feature, props.item)}
       />
       <label htmlFor={itemHash} className="feature__label">
         {props.name} ({USCurrencyFormat.format(props.cost)})
